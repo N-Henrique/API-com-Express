866d9bf85660858b3ba9f2e5248415e021c5f060
Henrique <n.n.henrique.n.n@gmail.com>
2023-07-29 14:13:15 -0300

    req DDL&DML no MySQL

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const express = require("express");
+const mysql = require("mysql2");
+
 const app = express();
 const PORT = 3000;
+
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "root",
+  database: "projetoapidados",
+  port: 3306,
+});
+
+// Operação DDL para criar uma tabela no banco de dados
+db.connect((err) => {
+  if (err) {
+    console.error("Deu erro ao conectar com o banco: ", err.message);
+  } else {
+    console.log("Conectado ao Banco MySQL.");
+
+    const createTable = `
+      CREATE TABLE IF NOT EXISTS usuarios (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        nome VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        idade INT NOT NULL
+      )
+    `;
+
+    db.query(createTable, (err, result) => {
+      if (err) {
+        console.error("Deu erro ao criar tabela: ", err.message);
+      } else {
+        console.log("Tabela 'usuarios' criada com sucesso!");
+      }
+    });
+  }
+});
 
 app.use(express.json());
 
@@ -15,7 +51,6 @@ app.get("/", (req, res) => {
     res.status(500).send(errorMessage);
   }
 });
-//http://localhost:3000
 
 // Rota POST
 app.post("/", (req, res) => {
@@ -25,22 +60,37 @@ app.post("/", (req, res) => {
     .status(201)
     .json({ message: `Rota POST: Você enviou a mensagem "${message}"` });
 });
-//localhost:3000
-//JSON {"message": "Mensagem de Teste"}
 
 // Rota PUT
 app.put("/", (req, res) => {
   console.log("Requisição PUT recebida.");
   res.status(200).send("Rota PUT: Requisição bem-sucedida!");
 });
-//localhost:3000
 
 // Rota DELETE
-http: app.delete("/", (req, res) => {
+app.delete("/", (req, res) => {
   console.log("Requisição DELETE recebida.");
   res.sendStatus(204);
 });
-//http://localhost:3000
+
+// Rota POST para adicionar usuário
+app.post("/usuarios", (req, res) => {
+  const { nome, email, idade } = req.body;
+
+  // Operação DML para inserir usuário na tabela
+  const insertUser = `
+    INSERT INTO usuarios (nome, email, idade) VALUES (?, ?, ?)
+  `;
+  db.query(insertUser, [nome, email, idade], (err, result) => {
+    if (err) {
+      console.error("Error ao adicionar usuario: ", err.message);
+      res.status(500).send("Error ao adicionar usuario.");
+    } else {
+      console.log("Usuario adicionado com sucesso!");
+      res.status(201).json({ message: "Usuario adicionado com sucesso!" });
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
